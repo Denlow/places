a2f9723b35079bbecbcebac0f444e0720b47702f
Dave Denlow and Jacob Hixon <jacob@>
2016-10-31 09:53:48 -0700

    Add form, Place constructor.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+// business logic
+
+function Place(location, landmarks, season) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#place-form").submit(function(event) {
+    event.preventDefault();
+
+    
+  })
+})
