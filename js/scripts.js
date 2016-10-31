be65c717c218d63e51959cbe7d40b646f5a55628
Dave Denlow and Jacob Hixon <dave@>
2016-10-31 11:13:00 -0700

    Add logic to clear input fields for each submit.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ function Place(place, location, landmark, season) {
 };
 
 // user interface logic
+
 $(document).ready(function() {
   $("form#place-form").submit(function(event) {
     event.preventDefault();
@@ -19,21 +20,20 @@ $(document).ready(function() {
     var place = $("input#place-name").val();
 
     var newPlace = new Place(place, location, landmark, season);
-    console.log(newPlace.landmark)
 
     $("ul#places-list").append("<li>" + "<span class='placeName'>" + newPlace.placeName + "</span>" + "</li>");
 
+    $("input[type=text], textarea").val("");
+
     $(".placeName").last().click(function() {
-    $("#show-place").show();
-    $("#show-place h2").text(newPlace.placeName);
-    $(".location").text(newPlace.location);
-    $(".landmark").text(newPlace.landmark);
-    $(".season").text(newPlace.season);
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeName);
+      $(".location").text(newPlace.location);
+      $(".landmark").text(newPlace.landmark);
+      $(".season").text(newPlace.season);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-
-
   });
 });
