a48ea2d0629fc7d51b2e3190d1a75d2f1092a44d
Dave Denlow and Jacob Hixon <jacob@>
2016-10-31 10:29:58 -0700

    Add logic to use Place constructor with user inputs. display list of Places. display Place info when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 
 // business logic
 
-function Place(location, landmarks, season) {
+function Place(place, location, landmark, season) {
   this.location = location;
-  this.landmarks = landmarks;
+  this.landmark = landmark;
   this.season = season;
+  this.placeName = place;
 };
 
 // user interface logic
@@ -12,6 +13,27 @@ $(document).ready(function() {
   $("form#place-form").submit(function(event) {
     event.preventDefault();
 
-    
-  })
-})
+    var location = $("input#location").val();
+    var season = $("input#season").val();
+    var landmark = $("input#landmark").val();
+    var place = $("input#place-name").val();
+
+    var newPlace = new Place(place, location, landmark, season);
+    console.log(newPlace.landmark)
+
+    $("ul#places-list").append("<li>" + "<span class='placeName'>" + newPlace.placeName + "</span>" + "</li>");
+
+    $(".placeName").last().click(function() {
+    $("#show-place").show();
+    $("#show-place h2").text(newPlace.placeName);
+    $(".location").text(newPlace.location);
+    $(".landmark").text(newPlace.landmark);
+    $(".season").text(newPlace.season);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+
+  });
+});
